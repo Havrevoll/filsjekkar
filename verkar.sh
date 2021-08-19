48869dfa28e6d0cc313ea26886730fc657aa94e9
Havrevoll <havrevoll@gmail.com>
2021-08-19 10:31:40 +0200

    La til endå ei line.

--- verkar.sh
+++ verkar.sh
@@ -3,3 +3,8 @@ jotta-cli.exe ls -l archive/Felles/Video/alle > jotta.txt
 for f in ./*.mts; do md5sum $f | perl -ple 's/ .*//' | if ! grep -q -f - jotta.txt; then echo $f; fi; done
 
 for f in ./*.mts; do md5sum $f | perl -ple 's/ .*//' | if ! grep -q -f - jotta.txt; then jotta-cli.exe archive $f --remote=Archive/Felles/Video/alle/$f --nogui; fi; done
+
+
+find . -type f -exec md5sum {} >> eth.txt \;
+perl -ple 's/ .*//' eth.txt > eth2.txt
+for f in $(cat eth2.txt); do if ! grep -q $f md5litteratur.txt; then grep $f eth.txt; fi;done
